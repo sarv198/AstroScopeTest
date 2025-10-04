a2d77fc2c9d11aed81405e9d6aedcce3475be303
Kobe Sam <kobesamx@gmail.com>
2025-10-04 13:04:19 -0400

    new results

--- utils/api_test.py
+++ utils/api_test.py
@@ -1,22 +1,28 @@
 import requests
-import pandas as pd
+# import pandas as pd
 
 start_date = '2015-06-30'
 end_date = '2016-06-30'
 api_key = 'V2PUUx21RDfaBSJn3VbX9AOsQHQ8b9b8qnT76OBY'
 
-print(f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={api_key}')
-result = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&api_key={api_key}')
+# print(f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&end_date={end_date}&api_key={api_key}')
+# result = requests.get(f'https://api.nasa.gov/neo/rest/v1/feed?start_date={start_date}&api_key={api_key}')
+
+result2 = requests.get(f'https://ssd-api.jpl.nasa.gov/cad.api?des=433&date-min=1900-01-01&date-max=2100-01-01&dist-max=0.2')
+# This API returns name, orbit ID, time of close-approach, approach distance, minimum distance, maximum distance, velocity relative to approach body, velocity relative to massless body, time of close approach, body, magnitude H, diameter, uncertainty in diameter, full name
+# We want TIME OF CLOSE-APPROACH, DISTANCE, VELOCITY RELATIVE TO APPROACH BODY, DIAMETER, FULLNAME
 
 try:
-    result.raise_for_status()
+    result2.raise_for_status()
 
 except requests.HTTPError:
-    print(f'Error {result.status_code}')
+    # print(f'Error {result.status_code}')
+    print(f'Error {result2.status_code}')
 
 #print(result.json())
+print(result2.json())
 
-print(result.json()['near_earth_objects'][start_date])
+# print(result.json()['near_earth_objects'][start_date])
 
 #df = pd.DataFrame(result.json()['near_earth_objects'])
 #print(df.head())
